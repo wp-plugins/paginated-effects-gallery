22753ba8cb4b538112d0cbcb890b2b9c08a12407
Nadeem Kelly <Nadeem Kelly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-12 16:25:49 +0000

    Fixed multiple galleries on 1 page bug
    git-svn-id: https://plugins.svn.wordpress.org/paginated-effects-gallery/tags/0.2@711847 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- paginated-effects-gallery.php
+++ paginated-effects-gallery.php
@@ -3,13 +3,13 @@
 Plugin Name: Paginated Effects Gallery
 Plugin URI: http://profiles.wordpress.org/nadeem-kelly
 Description: A simple, light and easy-to-use plugin that adds jQuery pagination to the standard wordpress gallery with cool jquery effects.
-Version: 0.1
+Version: 0.2
 Author: Nadeem Kelly
 Author URI: http://profiles.wordpress.org/nadeem-kelly
 License: GPL2
 */
 
-/*  Copyright 2013  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+/*  Copyright 2013  Nadeem Kelly  (email : nadeem.kelly@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -37,93 +37,101 @@ function add_this_script_footer() { ?>
 				var hideEffect = "<?php echo get_option( "hide_effect" ); ?>";
 				var showEffect = "<?php echo get_option( "show_effect" ); ?>";
 				var effectSpeed = "<?php echo get_option( "effect_speed" ); ?>";				
+				var count = 1;
 				
-				//select initial thumbnails to show
-				var totalThumbs = $( ".gallery-item" ).size();
-				var pagination = Math.ceil( totalThumbs / thumbsPerPage );
-				$( ".gallery-item" ).hide();
-				$( ".gallery-item" ).slice( 0, thumbsPerPage ).show();											
-				
-				//clean up break tags
-				$( ".gallery br" ).remove();
-				$( ".gallery-item:last" ).after( "<br style='clear:both;' />" );
-				
-				//wrap the gallery so it does does not collapse when we hide/show
-				$( ".gallery" ).wrap( "<div class='gallery-wrap'/>" );				
-				$( ".gallery-wrap" ).height( $( ".gallery" ).height() );	
-				
-				//generate our pagination
-				var html = "";
-				html += "<span id='pagination-first'>first</span> ";
-				for ( var i = 1; i < Math.min( maxPaginatedLinks + 1, pagination + 1 ); i++ ) {
-					var id = "pagination-" + i;
-					//determine the current selected page
-						var currentPage = "";
-						if( i == 1 ) {
-							currentPage += "class='current-page'";
-						}						
-						html += "<span id='" + id + "' " + currentPage + ">" + i + "</span> ";
-				}
-				html += "<span id='pagination-last'>last</span> ";				
-				$( ".gallery-wrap" ).after( "<div class='pagination'>" + html + "</div>" );
-				
-				//pagination callback function
-				$( "[id^=pagination]" ).live( "click", function() {						
-					var pagination_id = $( this ).attr( "id" ).substr( 11 );
-					var sliceFrom = 0;
+				//handles multiple galleries on page
+				var galleryCount = $( "[id^=gallery-]" ).each( function() {			
 					
-					//Determine thumbnails to show
-					if( pagination_id == "first" ) {
-						sliceFrom = 0;						
-						pagination_id = 1;
-					}
-					else if( pagination_id == "last" ) {
-						sliceFrom = ( pagination - 1 ) * thumbsPerPage;									
-						pagination_id = pagination;
-					}
-					else {
-						sliceFrom = ( pagination_id - 1 ) * thumbsPerPage;							
-					}
-									
-					//hide and show the new gallery		
-					if( hideEffect == "none" ) {
-						$( ".gallery" ).hide();
-						$( ".gallery-item" ).hide();
-						$( ".gallery-item" ).slice( sliceFrom, sliceFrom + thumbsPerPage ).show();
-						if( showEffect == "none" ) {
-							$( ".gallery" ).show();
-						}
-						else {
-							$( ".gallery" ).show( showEffect, effectSpeed );
-						}
-					}
-					else {
-						$( ".gallery" ).hide( hideEffect, effectSpeed, function() {
-							$( ".gallery-item" ).hide();
-							$( ".gallery-item" ).slice( sliceFrom, sliceFrom + thumbsPerPage ).show();
-							if( showEffect == "none" ) {
-								$( ".gallery" ).show();
-							}
-							else {
-								$( ".gallery" ).show( showEffect, effectSpeed );
-							}
-						});
-					}
+					var galleryId = "#" + $( this ).attr( "id" ) + " ";										
+				
+					//select initial thumbnails to show
+					var totalThumbs = $( galleryId + ".gallery-item" ).size();
+					var pagination = Math.ceil( totalThumbs / thumbsPerPage );
+					$( galleryId + ".gallery-item" ).hide();
+					$( galleryId + ".gallery-item" ).slice( 0, thumbsPerPage ).show();
 					
-					//update pagination
+					//clean up break tags
+					$( galleryId + " br" ).remove();
+					$( galleryId + ".gallery-item:last" ).after( "<br style='clear:both;' />" );
+					
+					//wrap the gallery so it does does not collapse when we hide/show
+					$( galleryId ).wrap( "<div class='gallery-wrap'/>" );				
+					$( galleryId ).parent().height( $( galleryId ).height() );	
+					
+					//generate our pagination
 					var html = "";
 					html += "<span id='pagination-first'>first</span> ";
-					for ( var i = Math.max( 1 , Math.min( pagination_id - Math.floor( maxPaginatedLinks / 2 ), pagination - ( maxPaginatedLinks - 1 ) ) ); i < Math.min( Math.max( 1, pagination_id - Math.floor( maxPaginatedLinks / 2) ) + maxPaginatedLinks, pagination + 1 ); i++ ) {
+					for ( var i = 1; i < Math.min( maxPaginatedLinks + 1, pagination + 1 ); i++ ) {
 						var id = "pagination-" + i;
 						//determine the current selected page
-						var currentPage = "";
-						if( pagination_id == i ) {
-							currentPage += "class='current-page'";
-						}						
-						html += "<span id='" + id + "' " + currentPage + ">" + i + "</span> ";
+							var currentPage = "";
+							if( i == 1 ) {
+								currentPage += "class='current-page'";
+							}						
+							html += "<span id='" + id + "' " + currentPage + ">" + i + "</span> ";
 					}
-					html += "<span id='pagination-last'>last</span> ";
-					$( ".pagination" ).html( html );
+					html += "<span id='pagination-last'>last</span> ";				
+					$( galleryId ).parent().after( "<div id='peg-" +  count +"' class='pagination'>" + html + "</div>" );
+					
+					//pagination callback function
+					$( "#peg-" + count ).find( "[id^=pagination]" ).live( "click", function() {								
+						var pagination_id = $( this ).attr( "id" ).substr( 11 );						
+						var sliceFrom = 0;						
+						
+						//Determine thumbnails to show
+						if( pagination_id == "first" ) {
+							sliceFrom = 0;						
+							pagination_id = 1;
+						}
+						else if( pagination_id == "last" ) {
+							sliceFrom = ( pagination - 1 ) * thumbsPerPage;									
+							pagination_id = pagination;
+						}
+						else {
+							sliceFrom = ( pagination_id - 1 ) * thumbsPerPage;							
+						}
+										
+						//hide and show the new gallery		
+						if( hideEffect == "none" ) {
+							$( galleryId ).hide();
+							$( galleryId + ".gallery-item" ).hide();
+							$( galleryId + ".gallery-item" ).slice( sliceFrom, sliceFrom + thumbsPerPage ).show();
+							if( showEffect == "none" ) {
+								$( galleryId ).show();
+							}
+							else {
+								$( galleryId ).show( showEffect, effectSpeed );
+							}
+						}
+						else {
+							$( galleryId ).hide( hideEffect, effectSpeed, function() {
+								$( galleryId + ".gallery-item" ).hide();
+								$( galleryId + ".gallery-item" ).slice( sliceFrom, sliceFrom + thumbsPerPage ).show();
+								if( showEffect == "none" ) {
+									$( galleryId ).show();
+								}
+								else {
+									$( galleryId ).show( showEffect, effectSpeed );
+								}
+							});
+						}
+						
+						//update pagination
+						var html = "";
+						html += "<span id='pagination-first'>first</span> ";
+						for ( var i = Math.max( 1 , Math.min( pagination_id - Math.floor( maxPaginatedLinks / 2 ), pagination - ( maxPaginatedLinks - 1 ) ) ); i < Math.min( Math.max( 1, pagination_id - Math.floor( maxPaginatedLinks / 2) ) + maxPaginatedLinks, pagination + 1 ); i++ ) {
+							var id = "pagination-" + i;
+							//determine the current selected page
+							var currentPage = "";
+							if( pagination_id == i ) {
+								currentPage += "class='current-page'";
+							}						
+							html += "<span id='" + id + "' " + currentPage + ">" + i + "</span> ";
+						}
+						html += "<span id='pagination-last'>last</span> ";					
+						$( "#peg-" + $( this ).parent().attr( "id" ).substr(4) ).html( html );					
+					});						
+					count++;
 				});					
 			});
 	</script><?php 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://profiles.wordpress.org/nadeem-kelly
 Tags: pagination, gallery, jQuery
 Requires at least: 3.5.1
 Tested up to: 3.5.1
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -65,3 +65,6 @@ This section will be updated when there is sufficient feedback for the plugin.
 
 = 0.1 =
 * Initial release
+
+= 0.2 =
+* Fixed bug for displaying multiple galleries on the same page
